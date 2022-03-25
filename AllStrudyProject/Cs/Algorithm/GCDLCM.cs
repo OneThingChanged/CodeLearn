989215d5c35917a20dd87408625e87f61451ee0d
River <38517532+OneThingChanged@users.noreply.github.com>
2022-03-25 11:48:27 +0900

    Initiate

---
+++ AllStrudyProject/Cs/Algorithm/GCDLCM.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cs.Algorithm
+{
+    //GCD = Greatest Common Divisor = 최대공약수;
+    //LCM = Least Common Multiple = 최소 공배수
+    public  static class GCDLCM
+    {
+        public static int GCD(int a, int b) {
+            if (b > a)
+            {
+                int tempSwap = a;
+                a = b;
+                b = tempSwap;
+            }
+            while (b != 0)
+            {
+                int r = a % b;
+                a = b;
+                b = r;
+            }
+            return a;
+        }
+
+        public static int LCM(int a, int b)
+        {
+            return a * b / GCD(a, b);
+        }
+    }
+}
